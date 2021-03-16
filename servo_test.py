9e63f52ff83219cae091f40f4eaeacdc36588181
GuigzLab <guillaume.dufau.pro@gmail.com>
2021-03-16 13:55:05 +0100

    motor_test.py

--- servo_test.py
+++ servo_test.py
@@ -4,10 +4,11 @@ import pwmio
 from adafruit_motor import servo
 
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A11, duty_cycle=2 ** 15, frequency=50)
-
+# pwm = pwmio.PWMOut(board.A11, duty_cycle=2 ** 15, frequency=50)
+kit = ServoKit(channels=16)
 # Create a servo object, my_servo.
-my_servo = servo.Servo(pwm)
+# my_servo = servo.Servo(pwm)
+my_servo = kit.servo[13]
 
 while True:
     for angle in range(45, 135, 5):  # 0 - 180 degrees, 5 degrees at a time.
